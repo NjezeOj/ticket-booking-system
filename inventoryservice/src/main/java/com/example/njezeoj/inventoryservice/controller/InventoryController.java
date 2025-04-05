19e0effcb72bba027d184668cae94a2084870409
njezeoj <njezeojinnaka@gmail.com>
2025-04-05 18:50:30 +0100

    feature: addition of inventoryGetAllEvents and inventoryByVenueId methods

--- inventoryservice/src/main/java/com/example/njezeoj/inventoryservice/controller/InventoryController.java
+++ inventoryservice/src/main/java/com/example/njezeoj/inventoryservice/controller/InventoryController.java
@@ -1,4 +1,28 @@
 package com.example.njezeoj.inventoryservice.controller;
 
+
+import com.example.njezeoj.inventoryservice.response.EventInventoryResponse;
+import com.example.njezeoj.inventoryservice.response.VenueInventoryResponse;
+import com.example.njezeoj.inventoryservice.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class InventoryController {
+
+    @Autowired
+    private InventoryService inventoryService;
+
+    @GetMapping("/inventory/events")
+    public @ResponseBody List<EventInventoryResponse> inventoryGetAllEvents(){
+        return inventoryService.getAllEvents();
+    }
+
+    @GetMapping("/inventory/venue/{venueId}")
+    public @ResponseBody VenueInventoryResponse inventoryByVenueId(@PathVariable("venueId") Long venueId) {
+        return inventoryService.getVenueInformation(venueId);
+    }
 }
